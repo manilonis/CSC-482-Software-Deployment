b61235c676d6929420127de28f86ac49b0ba1a32
manilonis <manilonis741@gmail.com>
2019-04-17 10:03:35 -0400

    latest server

--- src/HTTPS_Server/gorilla_server.go
+++ src/HTTPS_Server/gorilla_server.go
@@ -10,15 +10,20 @@ import(
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"strconv"
-	//"encoding/json"
 )
 
 func main(){
 	r := mux.NewRouter()
     r.HandleFunc("/maniloni/status", StatusHandler)
     r.HandleFunc("/maniloni/all", ListHandler)
+    r.HandleFunc("/maniloni/test", Tester)
     http.Handle("/", r)
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Tester(w http.ResponseWriter, r *http.Request){
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprint(w, "Server Runnign")
 }
 
 func ListHandler(w http.ResponseWriter, r *http.Request){
@@ -53,10 +58,7 @@ func ListHandler(w http.ResponseWriter, r *http.Request){
 
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
-    //vars := mux.Vars(r)
     w.WriteHeader(http.StatusOK)
-    //fmt.Println(vars)
-    //fmt.Fprint(w, "Hello World")
 
     sess, err := session.NewSession(&aws.Config{
     Region: aws.String("us-east-1")},)
@@ -85,15 +87,6 @@ func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := "{\"table\": \"subway\", \"ItemCount\":" + strconv.FormatInt(*(result.Table.ItemCount), 10) + "}"
-
-	//fmt.Fprint(w, result)
-	// fmt.Println(*(result.Table.ItemCount))
-
- //    bytes, err := json.Marshal(data)
- //    if err != nil {
- //        panic(err)
- //    }
-
     fmt.Fprint(w, data)
 	
 }
